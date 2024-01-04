5d48efd38d4091534824eacd232455805b8116ce
Aashis Pandey <aashispandey2080@gmai.com>
2024-01-04 16:38:31 +0545

    Changes content.

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
 /**
  * Main
  */
+
+
 public class Main {
+
+    public class Student {
+
+        public Student(){
+            System.out.println("HEllo from Stdent class constructor.");
+        }
+
+        public void sayHello(){
+            System.out.println("Hello from student class.");
+        }
+        
+    }
+    
 
     public static void main(String[] args){
        System.out.println("Jenkins automation server deployment.");
+      
     }
 }
